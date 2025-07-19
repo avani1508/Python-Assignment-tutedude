5f90b761bfa5ae3bc91f07af622dc34e101bd4c1
avani1508 <107167022+avani1508@users.noreply.github.com>
2025-07-19 16:54:52 +0530

    Add files via upload

--- PersonalizedGreeting.py
+++ PersonalizedGreeting.py
@@ -1,3 +1,4 @@
 Fname=input("Enter your first name: ")
 Lname=input("Enter your last name: ")
-print("Hello "+Fname,Lname+"! Welcome to python Program.")
+FullName=Fname+Lname
+print("Hello "+FullName+"! Welcome to python Program.")
